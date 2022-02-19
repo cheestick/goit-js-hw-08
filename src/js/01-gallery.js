0df36cf7864314973f4c94fd67b8332e0dbda1ad
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-02-19 17:21:33 +0200

    01-gallery completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+(function renderGallery() {
+  const galleryRef = document.querySelector('.gallery');
+  galleryRef.insertAdjacentHTML(
+    'afterbegin',
+    galleryItems
+      .map(
+        ({ preview, original, description }, index) =>
+          `<a class="gallery__item" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}"  />
+                    </a>`,
+      )
+      .join(''),
+  );
+})();
+
+const options = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+new SimpleLightbox('.gallery a', options);
