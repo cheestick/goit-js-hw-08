b0ba514e1afeb0736be0d89a6fc7e2dd57452512
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-02-20 00:23:02 +0200

    03-feedback mvp

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+const FEEDBACK_STATE_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form');
+const emailRef = formRef.querySelector('input[name="email"]');
+const messageRef = formRef.querySelector('textarea[name="message"]');
 
+const initialFormObj = JSON.parse(localStorage.getItem(FEEDBACK_STATE_KEY));
+
+emailRef.value = initialFormObj?.email ?? '';
+messageRef.value = initialFormObj?.message ?? '';
+
+formRef.addEventListener('input', e => {
+  const formElement = e.target;
+  const lsData = JSON.parse(localStorage.getItem(FEEDBACK_STATE_KEY)) ?? {};
+
+  lsData[formElement.name] = formElement.value;
+  localStorage.setItem(FEEDBACK_STATE_KEY, JSON.stringify(lsData));
+});
+
+formRef.addEventListener('submit', e => {
+  e.preventDefault();
+  const formObj = JSON.parse(localStorage.getItem(FEEDBACK_STATE_KEY)) ?? {};
+  console.log(formObj);
+  formRef.reset();
+  localStorage.removeItem(FEEDBACK_STATE_KEY);
+});
