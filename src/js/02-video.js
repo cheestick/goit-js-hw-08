c5838ae6f44086c409f5dbce56c9b96a71c8c7bd
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-02-20 18:30:52 +0200

    02-video done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
+import Vimeo from '@vimeo/player';
 
+const KEY_CURRENT_TIME = 'videoplayer-current-time';
+
+const playerFrame = document.querySelector('#vimeo-player');
+const player = new Vimeo(playerFrame);
+
+const dataLS = JSON.parse(localStorage.getItem(KEY_CURRENT_TIME));
+if (dataLS) {
+  player.setCurrentTime(dataLS);
+}
+
+player.on(
+  'timeupdate',
+  throttle(currentTime => {
+    localStorage.setItem(KEY_CURRENT_TIME, JSON.stringify(currentTime.seconds));
+  }, 1000),
+);
