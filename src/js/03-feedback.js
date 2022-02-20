004de708c180e1a666575edc01301f0f6cf192a2
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-02-20 17:52:51 +0200

    03-feedbacks done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,25 +1,40 @@
+import throttle from 'lodash.throttle';
+
 const FEEDBACK_STATE_KEY = 'feedback-form-state';
 const formRef = document.querySelector('.feedback-form');
 const emailRef = formRef.querySelector('input[name="email"]');
 const messageRef = formRef.querySelector('textarea[name="message"]');
 
-const initialFormObj = JSON.parse(localStorage.getItem(FEEDBACK_STATE_KEY));
+(function fillFormFieldsFromLS() {
+  const initialFormObj = JSON.parse(localStorage.getItem(FEEDBACK_STATE_KEY));
 
-emailRef.value = initialFormObj ? initialFormObj.email : '';
-messageRef.value = initialFormObj ? initialFormObj.message : '';
+  if (initialFormObj) {
+    emailRef.value = initialFormObj.email || '';
+    messageRef.value = initialFormObj.message || '';
+  }
+})();
 
-formRef.addEventListener('input', e => {
-  const formElement = e.target;
-  const lsData = JSON.parse(localStorage.getItem(FEEDBACK_STATE_KEY)) || {};
+formRef.addEventListener(
+  'input',
+  throttle(e => {
+    const formElement = e.target;
+    const lsData = JSON.parse(localStorage.getItem(FEEDBACK_STATE_KEY)) || {};
 
-  lsData[formElement.name] = formElement.value;
-  localStorage.setItem(FEEDBACK_STATE_KEY, JSON.stringify(lsData));
-});
+    lsData[formElement.name] = formElement.value;
+    localStorage.setItem(FEEDBACK_STATE_KEY, JSON.stringify(lsData));
+  }, 500),
+);
 
 formRef.addEventListener('submit', e => {
   e.preventDefault();
-  const formObj = JSON.parse(localStorage.getItem(FEEDBACK_STATE_KEY)) || {};
-  console.log(formObj);
+  getFormData(formRef);
   formRef.reset();
   localStorage.removeItem(FEEDBACK_STATE_KEY);
 });
+
+function getFormData(form) {
+  const formData = Object.create(null);
+  new FormData(form).forEach((value, field) => (formData[field] = value));
+  console.log(formData);
+  return formData;
+}
